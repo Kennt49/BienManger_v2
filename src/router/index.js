d60a2573626e6db0453d6674befab699bac66202
jyr <jean-yves.rouseau@orange.fr>
2022-09-13 15:06:36 +0200

    suppression page non util

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 import retourDataView from '@/views/retourDataView'
 import RecetteUniqView from '@/views/RecetteUniqView'
 import ajoutView from '@/views/ajoutView'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
+
   {
     path: '/retourData',
     name: 'retourData',
@@ -25,14 +21,7 @@ const routes = [
     name: 'recette',
     component: ajoutView
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+
 ]
 
 const router = createRouter({
