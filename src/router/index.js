f0ba33ca12638fdd4d99f18e96121ce7bc7035ac
Kent <kenneth.g@gmail.com>
2022-09-14 09:41:55 +0200

    creation supprimerview

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import retourDataView from '@/views/retourDataView'
 import RecetteUniqView from '@/views/RecetteUniqView'
 import ajoutView from '@/views/ajoutView'
+import supprimerView from '@/views/SupprimerView'
 
 const routes = [
   {
@@ -32,6 +33,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/supprimer',
+    name: 'supprimer',
+    component: supprimerView
   }
 ]
 
