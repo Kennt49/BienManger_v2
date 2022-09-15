cdc4d24be29ce63871d0a5847e6565c6863cfff0
Kent <kenneth.g@gmail.com>
2022-09-15 12:04:00 +0200

    supprimer view et css change

--- src/views/SupprimerView.vue
+++ src/views/SupprimerView.vue
@@ -1,10 +1,10 @@
 <template>
 
-    <body class="back">
+    <body class="back-img-cover">
 
         <VignetteNavbar />
 
-        <div class="recette-supr">
+        <div class=" recette-supr">
             <div class="tab-recette">
                 <div class="recette-nom-tab">
                     <p class="nom-recette-supr">Omelette au fromage</p>
@@ -15,16 +15,42 @@
                 </div>
             </div>
         </div>
-        <div>
-            <p>Êtes-vous sûr de vouloir supprimer cette recette ?</p>
-        </div>
-        <div class="choix">
-            <div class="display">
-                <button type="button" class="btn display btn-outline-primary"><i
-                        class="bi bi-arrow-left-circle-fill"></i></button>
-            </div>
-            <div class="display">
-                <button type="button" class="">Oui, je suis sûr</button>
+        <div class="box-choix-supr">
+            <div class="champs-choix-supr">
+                <div class="text-choix-supr bold">
+                    <p>Êtes-vous sûr de vouloir supprimer cette recette ?</p>
+                </div>
+                <div class="choix">
+                    <div class="display">
+                        <button type="button" class="btn display btn-outline-primary"><i
+                                class="bi bi-caret-left"></i></button>
+                    </div>
+                    <div class="display">
+                        <button type="button" class="bt-confirmation-supr" data-bs-toggle="modal"
+                            data-bs-target="#modal1">Oui, je suis sûr</button>
+
+                        <!--Modal-->
+                        <div class="modal fade" data-bs-backdrop="static" id="modal1">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" aria-labelledby="suppression confirmée">
+                                            Confirmation
+                                        </h5>
+                                    </div>
+                                    <div class="modal-body" aria-describedby="content"
+                                        aria-labelledby="la recette a bien été supprimée">
+                                        <p>La recette a bien été supprimée</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button class="btn btn-primary text-white"
+                                            aria-labelledby="Retour à la page d'acceuille">OK</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
