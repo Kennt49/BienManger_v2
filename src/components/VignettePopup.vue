7bb68aba7de6b1ba6382473f0b7696d429969d02
Kent <kenneth.g@gmail.com>
2022-09-16 11:07:11 +0200

    petit changement

--- src/components/VignettePopup.vue
+++ src/components/VignettePopup.vue
@@ -1,53 +1,50 @@
 <template>
-    <div class="display margin-up">
-        <button type="button" class="bt-confirmation-supr" data-bs-toggle="modal" data-bs-target="#modal1">Oui, je suis
-            sûr</button>
 
-        <!--Modal-->
-        <div class="modal fade" data-bs-backdrop="static" id="modal1">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" aria-labelledby="confirmation de suppression">
-                            Confirmation
-                        </h5>
-                    </div>
-                    <div class="modal-body" aria-describedby="content"
-                        aria-labelledby="voulez-vous vraiment supprimer cette recette">
-                        <p>Voulez-vous vraiment supprimer cette recette ?</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-primary text-white"
-                            aria-labelledby="Annule l'action de suppression de la recette"
-                            data-bs-dismiss="modal">Non</button>
-                        <button class="btn btn-primary text-white"
-                            aria-labelledby="Confirme l'action de suppression de la recette" data-bs-toggle="modal"
-                            data-bs-target="#modal2">Oui</button>
-                    </div>
+
+    <!--Modal-->
+    <div class="modal fade" data-bs-backdrop="static" id="modal1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" aria-labelledby="confirmation de suppression">
+                        Confirmation
+                    </h5>
                 </div>
-            </div>
-        </div>
-        <!--Modal-->
-        <div class="modal fade" data-bs-backdrop="static" id="modal2">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" aria-labelledby="suppression confirmée">
-                            Confirmation
-                        </h5>
-                    </div>
-                    <div class="modal-body" aria-describedby="content"
-                        aria-labelledby="La recette a bien été supprimée !">
-                        <p>La recette a bien été supprimée !</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-primary text-white" aria-labelledby="Retour à l'acceuil"
-                            data-bs-dismiss="modal" @click="vers">OK</button>
-                    </div>
+                <div class="modal-body" aria-describedby="content"
+                    aria-labelledby="voulez-vous vraiment supprimer cette recette">
+                    <p>Voulez-vous vraiment supprimer cette recette ?</p>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary text-white"
+                        aria-labelledby="Annule l'action de suppression de la recette"
+                        data-bs-dismiss="modal">Non</button>
+                    <button class="btn btn-primary text-white"
+                        aria-labelledby="Confirme l'action de suppression de la recette" data-bs-toggle="modal"
+                        data-bs-target="#modal2">Oui</button>
                 </div>
             </div>
         </div>
     </div>
+    <!--Modal-->
+    <div class="modal fade" data-bs-backdrop="static" id="modal2">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" aria-labelledby="suppression confirmée">
+                        Confirmation
+                    </h5>
+                </div>
+                <div class="modal-body" aria-describedby="content" aria-labelledby="La recette a bien été supprimée !">
+                    <p>La recette a bien été supprimée !</p>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary text-white" aria-labelledby="Retour à l'acceuil"
+                        data-bs-dismiss="modal" @click="vers">OK</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
